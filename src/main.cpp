c83ee3123236e3abfdf94ce26d4e63a899c806be
blackpill0w <blackpill0w@protonmail.com>
2023-01-30 18:51:46 +0100

    Add raylib & fmt

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,26 @@
 #include <SFML/Window.hpp>
+#include <raylib.h>
+#include <fmt/color.h>
 
 int main() {
    sf::Window win{ sf::VideoMode(600, 600), "SFML" };
 
-   while (win.isOpen()) {
+   while (win.isOpen())
+   {
       sf::Event e;
       while (win.pollEvent(e)) {
          if (e.type == sf::Event::Closed) win.close();
       }
    }
+
+   InitWindow(600, 600, "Raylib");
+   while (!WindowShouldClose())
+   {
+      BeginDrawing();
+      ClearBackground(GRAY);
+      EndDrawing();
+   }
+   CloseWindow();
+
+   fmt::print(fg(fmt::color::green) | fmt::emphasis::bold, "Hello world!\n");
 }
